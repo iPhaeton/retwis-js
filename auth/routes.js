205ed6256c4092c8bddc566a53a9eda52c903376
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-09-25 11:28:16 +0300

    Add body parser

--- auth/routes.js
+++ auth/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());``
 
 router.post('/signup', (req, res, next) => {
-    console.log('signup');
+    console.log('signup', req.body);
     res.send('ok');
 });
 
